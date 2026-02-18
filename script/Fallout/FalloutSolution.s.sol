7d5244a8d891e7f4c7293b089e788d67427de07c
Patrick-Ngururi <patrick9ngururi8@gmail.com>
2026-02-18 17:53:42 +0300

    Solc version updates

--- script/Fallout/FalloutSolution.s.sol
+++ script/Fallout/FalloutSolution.s.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.8.0;
+pragma solidity ^0.6.0;
 
-import {Fallback} from "src/Fallout/Fallout.sol";
-import {Script} from "lib/forge-std/src/Script.sol";
-import {console} from "lib/forge-std/src/console.sol";
+import {Fallback} from "../../src/Fallout/Fallout.sol";
+import {Script} from "../../lib/forge-std/src/Script.sol";
+import {console} from "../../lib/forge-std/src/console.sol";
 
 contract FalloutSolution is Script {
-    Fallout public falloutInstance = Fallback(0x0560756aF3C6E55aa457e53EC07E7bB04e4d98B8);
+    Fallout public falloutInstance = Fallout(0x0560756aF3C6E55aa457e53EC07E7bB04e4d98B8);
 
     function run() external {
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
