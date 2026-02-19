513798f065687502564b296f28b3614c04569528
Patrick-Ngururi <patrick9ngururi8@gmail.com>
2026-02-19 18:21:35 +0300

    Incompatible Solc version for the Fallout challenge

--- script/Fallback/Fallback.s.sol
+++ script/Fallback/Fallback.s.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.8.0;
+pragma solidity ^0.8.0;
 
-import {Fallback} from "src/Fallback/Fallback.sol";
-import {Script} from "lib/forge-std/src/Script.sol";
-import {console} from "lib/forge-std/src/console.sol";
+import {Fallback} from "../../src/Fallback/Fallback.sol";
+import {Script} from "../../lib/forge-std/src/Script.sol";
+import {console} from "../../lib/forge-std/src/console.sol";
 
 contract FallbackSolution is Script {
     Fallback public fallbackInstance = Fallback(payable(0xCBE0B9bd2eb04269A8f70b8788c01f0175b2F578));
--- script/Fallout/FalloutSolution.s.sol
+++ script/Fallout/FalloutSolution.s.sol
@@ -2,14 +2,14 @@
 
 pragma solidity ^0.6.0;
 
-import {Fallback} from "../../src/Fallout/Fallout.sol";
+import {Fallout} from "../../src/Fallout/Fallout.sol";
 import {Script} from "../../lib/forge-std/src/Script.sol";
 import {console} from "../../lib/forge-std/src/console.sol";
 
 contract FalloutSolution is Script {
-    Fallout public falloutInstance = Fallout(0x0560756aF3C6E55aa457e53EC07E7bB04e4d98B8);
+    Fallout public falloutInstance = Fallout(payable(0x0560756aF3C6E55aa457e53EC07E7bB04e4d98B8));
 
-    function run() external {
+    function run() public {
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
 
         console.log("Owner before: ", falloutInstance.owner());
